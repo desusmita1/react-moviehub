5369e95cdc7d2cbead7a5d7224554e7968f337c1
Susmita De <desusmita001@gmail.com>
2025-03-04 21:42:11 -0600

    Updating changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import '../styles.css';
+import React from "react";
+import "../styles.css";
 
-
-export default function Header(){
-    return (
-        <div className='header'>
-            <img className='logo' src='logo1.png' alt='moviehub'/>
-            <h2 className='app-subtitle'>It's time for popcorn! Find your next movie here.</h2>
-        </div>
-    );
-
+export default function Header() {
+  return (
+    <div className='header'>
+      <img className='logo' src='logo1.png' alt='moviehub' />
+      <h2 className='app-subtitle'>
+        It's time for popcorn! Find your next movie here.
+      </h2>
+    </div>
+  );
 }
